cfa91d0a2661a66082d6a166eb06dcd14ec995b6
Kyvan <kyvanemami@yahoo.ca>
2017-05-12 15:02:53 -0400

    Rearranging the order of execution

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -12,17 +12,7 @@
 #		phpMyAdmin		  #
 ###########################################
 
-yum install -y httpd wget yum-utils
-
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
-rpm -ivh mysql-community-release-el7-5.noarch.rpm
-
-yum update -y
-
-yum install -y epel-release mysql-server php
+yum install -y wget yum-utils git
 
 read -p "Are you installing PLESK or phpMyAdmin? (Plesk/PHP)" choice
 
@@ -30,10 +20,25 @@ if [ ${$choice,,} == "plesk" ] ; then
 	wget http://autoinstall.plesk.com/plesk-installer
 	chmod +x plesk-installer
 	./plesk-installer
+
 elif [ ${$choice,,} == "php" ] ; then
+	wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+	wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+	wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+	
+	rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+	rpm -ivh mysql-community-release-el7-5.noarch.rpm
+	
 	echo -e "Which version of PHP do you want to install?\nPlease put the first to digist without any dots or dashes.\nExample for version 5.6.XX: 56"
 	read phpv
+	
 	yum-config-manager --enable remi-php$phpv
+	
+	yum clean all
+	yum update -y
+	yum install -y httpd epel-release mysql-server
+
+	
 	yum install -y php phpmyadmin
 else
 	echo -e "Your answer doesn't match any of the options.\nSo I guess we are done here."
