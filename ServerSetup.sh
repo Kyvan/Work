b1aeeea5f78531bc2567cac32c799b7a282a2b65
Kyvan <kyvanemami@yahoo.ca>
2017-05-12 12:21:47 -0400

    Script to setup a Linux Box

---
+++ ServerSetup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -u
+
+###########################################
+#	   Made by Kyvan Emami		  #
+#	 DATE:	  May 12th, 2017	  #
+#	Script to setup Linux Boxes	  #
+#	    Services include:		  #
+#		  HTTPD			  #
+#		   PHP			  #
+#		  MySQL			  #
+#		  Plesk			  #
+#		phpMyAdmin		  #
+###########################################
+
+yum install -y httpd wget yum-utils
+
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+
+yum update -y
+
+yum install -y epel-release mysql-server php
+
+read -p "Are you installing PLESK or phpMyAdmin? (Plesk/PHP)" choice
+
+if [ ${$choice,,} == "plesk" ] ; then
+	wget http://autoinstall.plesk.com/plesk-installer
+	chmod +x plesk-installer
+	./plesk-installer
+elif [ ${$choice,,} == "php" ] ; then
+	echo -e "Which version of PHP do you want to install?\nPlease put the first to digist without any dots or dashes.\nExample for version 5.6.XX: 56"
+	read phpv
+	yum-config-manager --enable remi-php$phpv
+	yum install -y php phpmyadmin
+else
+	echo -e "Your answer doesn't match any of the options.\nSo I guess we are done here."
+fi
