fb05ccab3c6ed8d4acca6c09d07cc3bda9041233
Kyvan <kyvanemami@yahoo.ca>
2017-06-21 10:54:37 -0400

    Code optimized

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -19,38 +19,50 @@ systemctl disable firewalld
 
 sed -i 's/#//' /etc/ssh/sshd_config
 sed -i 's/22/10022/' /etc/ssh/sshd_config
-
 sed -i 's/22/10022/' /etc/sysconfig/iptables
 
 systemctl restart sshd
-systemctl restart itpables
+systemctl start itpables
 systemctl enable iptables
 
-read -p "Are you installing PLESK or phpMyAdmin? (Plesk/PHP)" choice
+read -p "Are you installing PLESK or phpMyAdmin? (Plesk/PHP) " choice
 
 if [ ${$choice,,} == "plesk" ] ; then
 	wget http://autoinstall.plesk.com/plesk-installer
 	chmod +x plesk-installer
 	./plesk-installer
 
-elif [ ${$choice,,} == "php" ] ; then
-	wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-	wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-	
-	rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
-	rpm -ivh mysql-community-release-el7-5.noarch.rpm
-	
-	echo -e "Which version of PHP do you want to install?\nPlease put the first to digist without any dots or dashes.\nExample for version 5.6.XX: 56"
-	read phpv
-	
-	yum-config-manager --enable remi-php$phpv
-	
+elif [ {$choice,,} == "php" ] ; then
+	read -p "Are you using CentOS 6 or 7? (6/7) " version
+	if $version eq 7 ; then 
+		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+		wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+		wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+		
+		rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+		rpm -ivh mysql-community-release-el7-5.noarch.rpm
+		
+		echo -e "Which version of PHP do you want to install?\nPlease put the first to digist without any dots or dashes.\nExample for version 5.6.XX: 56"
+		read phpv
+		
+		yum-config-manager --enable remi-php$phpv
+	elif $version eq 6 ; then
+		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+		wget http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+		wget http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
+		
+		rpm -Uvh remi-release-7.rpm epel-release-latest-6.noarch.rpm
+		rpm -ivh mysql-community-release-el6-5.noarch.rpm
+		
+		echo -e "Which version of PHP do you want to install?\nPlease put the first to digist without any dots or dashes.\nExample for version 5.6.XX: 56"
+		read phpv
+		
+		yum-config-manager --enable remi-php$phpv
+	fi
+
 	yum clean all
 	yum update -y
-	yum install -y httpd epel-release mysql-server
-
-	yum install -y php phpmyadmin
+	yum install -y httpd epel-release mysql mysql-server php phpmyadmin 
 	
 	systemctl start mysqld
 	systemctl start httpd
