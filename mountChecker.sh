7f0db3af99b8a76c358c5818436ac8a6a35e901c
Kyvan <kyvanemami@yahoo.ca>
2018-09-11 11:08:46 -0400

    Mount Checker Script
    Checks the number of mapped drives, and mapps them if they are not all
    mapped.

---
+++ mountChecker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -u
+
+# Variable to hold the humber of mounted shares
+mountNum=`mount | grep -ci "datadump"`
+
+# If statement, to check and see if the number of mapped drives are correct,
+# if not, it will map everything again.
+if [[ $mountNum -ne `mount | grep -ic 'datadump' /etc/fstab` ]] ; then
+        mount -a
+fi
