3f92189e781a2ac7a7b783bba5d6370cd2e5e928
Kyvan Emami <kyvanemami@yahoo.ca>
2019-04-22 15:41:29 -0400

    Variable update

--- mountChecker.sh
+++ mountChecker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -u
 
-# Variable to hold the humber of mounted drives
+# Variable to check the humber of mounted drives
 mountNum=`mount | grep -ci "datadump"`
+
+# Variable to check the number of drives to be mapped
+mounted=`mount | grep -ic 'datadump' /etc/fstab`
 
 # If statement, to check and see if the number of mapped drives are correct,
 # if not, it will map everything again.
-if [[ $mountNum -ne `mount | grep -ic 'datadump' /etc/fstab` ]] ; then
+if [[ $mountNum -ne $mounted ]] ; then
         mount -a
 fi
