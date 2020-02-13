d1211331a70c9301b7a14f64fafbc0cb17d235fc
kyvan <kyvanemami@yahoo.ca>
2020-02-13 15:00:26 -0500

    Update

--- userCreation.sh
+++ userCreation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ux
 
+myHome=`who | awk '{print $1}'`
+
 # Ask how many users are being created
 read -p "How many users do you need to make? " userCount
-
-myHome=`who | awk '{print $1}'`
 
 # For loop to make the users, add them to the proper group
 # and generate RSA keys for them
@@ -20,15 +20,15 @@ for (( i = 0 ; i < $userCount ; i++ )) ; do
         pass=$(perl -e 'print crypt($ARGV[0], "password")' $userPass)
         useradd $userName -G sudo -md /home/$userName -p $pass -s /bin/bash
 
-        # Makes upload and .ssh folders in user home directory
-        mkdir $userHome/upload $userHome/.ssh
+        # Makes .ssh folders in user home directory
+        mkdir $userHome/.ssh
 
         # Generate RSA key and renames the public to the correct name
         ssh-keygen -t rsa -N "" -f $userHome/.ssh/"$userName"_rsa
         mv $userHome/.ssh/"$userName"_rsa.pub $userHome/.ssh/authorized_keys
 
         # Changes ownership user folders
-        chown -R $userName:$userName $userHome/.ssh $userHome/upload
+        chown -R $userName:$userName $userHome/.ssh
 
         # Change the permissions for the .ssh folder and RSA keys
         chmod 0700 $userHome/.ssh $userHome/.ssh/authorized_keys
