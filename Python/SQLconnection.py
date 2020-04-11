7efea9d8a3d3e9936f797b2efca71511c167b09e
Kyvan Emami <kyvanemami@yahoo.ca>
2020-04-11 18:48:32 -0400

    Commented out a section

--- Python/SQLconnection.py
+++ Python/SQLconnection.py
@@ -6,8 +6,10 @@ password = 'password'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
+"""
 cursor.execute("SELECT * from *;") 
 row = cursor.fetchone() 
 while row: 
     print(row[0])
     row = cursor.fetchone()
+"""
