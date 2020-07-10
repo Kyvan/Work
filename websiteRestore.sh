2396fe1f1113591a7da73aaa13a5043cd182332d
kyvan <kyvanemami@yahoo.ca>
2020-07-10 16:24:21 -0400

    Modified to be more automated

--- websiteRestore.sh
+++ websiteRestore.sh
@@ -7,6 +7,12 @@ tarName="eastrockClean.tar.gz"
 # Goes to website root directory
 cd $rootDir
 
+# Suspend the domain
+plesk bin domain --suspend eastrock.com
+
+# Kills all the infected sessions running for eastrock.com
+ps aux | grep  webmas | grep -v color | awk '{ print $2 }' | xargs kill -9
+
 # Loop to remove all files except the tar file
 for fileName in * ; do
     if [[ $fileName != $tarName ]] ; then
@@ -22,3 +28,6 @@ chown -R webmaster:psacln ./* ./.??*
 find ./ -type d -exec chmod 0755 {} \;
 find ./ -type f -exec chmod 0644 {} \;
 chmod 0400 wp-config.php
+
+# Activate the domain
+plesk bin domain --on eastrock.com
