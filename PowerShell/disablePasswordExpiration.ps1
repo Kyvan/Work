7be0855af8c7ce4674e67fae3b832143cde04968
Kyvan Emami <emamikyvan@gmail.com>
2021-01-31 09:40:39 -0500

    Variable changes

--- PowerShell/disablePasswordExpiration.ps1
+++ PowerShell/disablePasswordExpiration.ps1
@@ -1,5 +1,5 @@
 $emailAddress = Read-Host "What is the email address you want to disable password expiration for? "
-$Cred = Get-Credential
-Connect-AzureAD -Credential $Cred
+$cred = Get-Credential
+Connect-AzureAD -Credential $cred
 
 Set-AzureADUser -ObjectId $emailAddress -PasswordPolicies DisablePasswordExpiration
--- PowerShell/disablePasswordExpirationForAll.ps1
+++ PowerShell/disablePasswordExpirationForAll.ps1
@@ -1,5 +1,5 @@
-$Cred = Get-Credential
-Connect-AzureAD -Credential $Cred
+$cred = Get-Credential
+Connect-AzureAD -Credential $cred
 
 # disbales password expiry for all users in the organization
 Get-AzureADUser -All $true | Set-AzureADUser -PasswordPolicies DisablePasswordExpiration
