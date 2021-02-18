4e73073d90f4baddc9dbc7b1053b195460a16e37
Kyvan Emami <emamikyvan@gmail.com>
2021-02-18 13:40:55 -0500

    Fixed text formatting issue

--- PowerShell/setDKIM.ps1
+++ PowerShell/setDKIM.ps1
@@ -1,15 +1,15 @@
-$domainName = Read-Host "What is the domain name? "
-$Cred = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Cred -Authentication Basic -AllowRedirection
-Import-PSSession $Session
+$domainName = Read-Host "What is the domain name? "
+$Cred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Cred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
 
-# Makes sure DKIM is disabled for the domain and then grabs the DNS records needed from Office365
-New-DkimSigningConfig -DomainName $domainName -Enabled $false
-Get-DkimSigningConfig -Identity $domainName | Format-List Selector1CNAME, Selector2CNAME
+# Makes sure DKIM is disabled for the domain and then grabs the DNS records needed from Office365
+New-DkimSigningConfig -DomainName $domainName -Enabled $false
+Get-DkimSigningConfig -Identity $domainName | Format-List Selector1CNAME, Selector2CNAME
 Pause
 
-# Only run this after the DNS records from the steps above are added
-Set-DkimSigningConfig -Identity $domainName -Enabled $true
+# Only run this after the DNS records from the steps above are added
+Set-DkimSigningConfig -Identity $domainName -Enabled $true
 Pause
 
-Remove-PSSession $Session
+Remove-PSSession $Session
