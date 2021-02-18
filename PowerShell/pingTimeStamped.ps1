224de3ec79ac2d929d1b6775b179077ac86ef8ec
Kyvan Emami <emamikyvan@gmail.com>
2021-02-18 08:47:01 -0500

    Fixed username variable issue

--- PowerShell/pingTimeStamped.ps1
+++ PowerShell/pingTimeStamped.ps1
@@ -1,1 +1,1 @@
-ping -t 8.8.8.8 | ForEach-Object{"{0} - {1}" -f (Get-Date),$_} > c:\users\%username%\desktop\ping_log.txt
+ping -t 8.8.8.8 | ForEach-Object{"{0} - {1}" -f (Get-Date),$_} > c:\users\$env:USERNAME\desktop\ping_log.txt
