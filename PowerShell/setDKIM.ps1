32599c99462ce2fb02709fb21b49a2df02127b45
Kyvan Emami <emamikyvan@gmail.com>
2021-02-18 14:02:48 -0500

    Added some comments

--- PowerShell/setDKIM.ps1
+++ PowerShell/setDKIM.ps1
@@ -6,6 +6,9 @@ Import-PSSession $Session
 # Makes sure DKIM is disabled for the domain and then grabs the DNS records needed from Office365
 New-DkimSigningConfig -DomainName $domainName -Enabled $false
 Get-DkimSigningConfig -Identity $domainName | Format-List Selector1CNAME, Selector2CNAME
+
+Write-Host "host name for the DNS records are 'selector1._domainkey' and 'selector2._domainkey'."
+Write-Host "Please wait for about 5 to 10 minutes after making the new DNS records before moving forward to ensure DNS has propegated."
 Pause
 
 # Only run this after the DNS records from the steps above are added
