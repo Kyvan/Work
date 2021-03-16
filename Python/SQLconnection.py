db2bc9c4a16523a80533b8ba24ae6276df50f5dd
Kyvan Emami <emamikyvan@gmail.com>
2021-03-16 13:29:12 -0400

    Changes DB connector module

--- Python/SQLconnection.py
+++ Python/SQLconnection.py
@@ -1,15 +1,26 @@
-import pyodbc
-server = '127.0.0.1'
-database = 'testDB'
-username = 'username'
-password = 'password'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+import mysql.connector
+from mysql.connector.constants import ClientFlag
+
+config = {
+    'user': 'Dawn',
+    'password': 'Diesel123',
+    'host': '10.200.21.42',
+    'database': 'delsqlserver',
+    'port': "1433",
+#   'client_flags': [ClientFlag.SSL],
+#   'ssl_ca': '/opt/mysql/ssl/ca.pem',
+#   'ssl_cert': '/opt/mysql/ssl/client-cert.pem',
+#   'ssl_key': '/opt/mysql/ssl/client-key.pem',
+}
+
+cnxn = mysql.connector.connect(**config)
 cursor = cnxn.cursor()
 
-"""
+
 cursor.execute("SELECT * from *;") 
 row = cursor.fetchone() 
 while row: 
     print(row[0])
     row = cursor.fetchone()
-"""
+
+#cnxn.close()
