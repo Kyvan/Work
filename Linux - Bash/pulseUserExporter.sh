dfc3499b2f6cda1ba8cd7bdf6d5424ab0c2ef212
Kyvan Emami <emamikyvan@gmail.com>
2021-04-19 16:35:52 -0400

    Modified locations and loops

--- Linux - Bash/pulseUserExporter.sh
+++ Linux - Bash/pulseUserExporter.sh
@@ -7,7 +7,7 @@ function xmlParser() {
 }
 
 function userExporter() {
-    xmlDir="c:\users\kyvan\OneDrive - dnsnetworks.ca\XML"
+    xmlDir="c:\users\kyvan\OneDrive - dnsnetworks.ca\XML\Users"
     cd "$xmlDir" || exit
     
     for xmlReports in * ; do
@@ -15,34 +15,34 @@ function userExporter() {
             if [ "$ENTITY" = "username" ] || [ "$ENTITY" = "fullname" ] || [ "$ENTITY" = "enabled" ] ; then
                 echo "$ENTITY => $CONTENT"
             fi
-        done < "$1" > exportedUsers\\"$1".txt
-    done
-    
-    echo -e "\e[34mDMARC report for $1 is ready."
+        done < "$xmlReports" > ../exportedUsers\\"$xmlReports".txt
+        echo -e "\e[34mVPN user list for $xmlReports is ready."
+    done    
 }
 
 function enabledVPNUsers() {
     exportedDir="c:\users\kyvan\OneDrive - dnsnetworks.ca\XML\exportedUsers"
     cd "$exportedDir" || exit
 
-    find . -maxdepth 1 -daystart -mtime -1 -type f -exec grep -B2 -i true {} \; >> Enabled/enabledVPNUsers-"$(date +%F)".txt
+    find . -maxdepth 1 -daystart -mtime -1 -type f -exec grep -B2 -i true {} \; > ../Enabled/enabledVPNUsers-"$(date +%F)".txt
     echo -e "\e[35mEnabled VPN Users are ready."
 }
 
 function countVPNUsers() {
-    enabledDir="c:\users\kyvan\OneDrive - dnsnetworks.ca\XML\exportedUsers\Enabled"
+    enabledDir="c:\users\kyvan\OneDrive - dnsnetworks.ca\XML\Enabled"
     cd "$enabledDir" || exit
 
-    grep -i fullname enabledVPNUsers* | awk '{ print $NF }' | sort | uniq -c >> ../Counted/countedVPNUsers-"$(date +%F)".txt
-    echo -e "\e[35mEnabled VPN Users are ready."
+    grep -i fullname enabledVPNUsers* | awk '{ print $NF }' | sort | uniq -c > ../Counted/countedVPNUsers-"$(date +%F)".txt
+    echo -e "\e[36mCount of VPN Users is ready."
 }
 
 function options() {
     echo "These are the options that can be used with this script (only one option at a time)"
     echo "PureColo: Export Users for PureColo VPN appliance"
     echo "Rogers: Export Users for Rogers/Miami VPN appliance"
+    echo "Montreal: Export Users for Montreal VPN appliance"
     echo "Count: Count number of VPN accounts per customer"
-    echo "Both: Export Users for all VPN appliances"
+    echo "All: Export Users for all VPN appliances"
 }
 
 function chosenOption() {
@@ -50,8 +50,8 @@ function chosenOption() {
         --help | -h | help | h)
             options
             ;;
-        both)
-            echo "Generating ALL VPN Users" && userExporter PureColo.xml && userExporter Rogers.xml && enabledVPNUsers
+        all)
+            echo "Generating ALL VPN Users" && userExporter && enabledVPNUsers && countVPNUsers #Montreal.xml && userExporter PureColo.xml && userExporter Rogers.xml && enabledVPNUsers
             ;;
         count)
             echo "Generating counted VPN account report" && countVPNUsers
@@ -61,6 +61,9 @@ function chosenOption() {
             ;;
         rogers)
             echo "Generating Rogers/Miami VPN Users" && userExporter Rogers.xml
+            ;;
+        montreal)
+            echo "Generating Montreal VPN Users" && userExporter Montreal.xml
             ;;
         *)
             echo "If you need to know the options, please run the script with --help"
